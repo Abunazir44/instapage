ed6a2827add7bc33f107623da6ecf1923b09505d
Abunazir44 <silaslinkrock2@gmail.com>
2025-05-09 18:05:08 -0300

    Atualização_10

--- firebase-config.js
+++ firebase-config.js
@@ -25,12 +25,12 @@ import {
 
 // 🔧 Substitua pelas suas credenciais do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seuprojeto.firebaseapp.com",
-  projectId: "seuprojeto",
-  storageBucket: "seuprojeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyBKf6qUHaXQOHvgLn6FvIAUgJ1pTzKr6jk",
+  authDomain: "instapage-mini-saas.firebaseapp.com",
+  projectId: "instapage-mini-saas",
+  storageBucket: "instapage-mini-saas.firebasestorage.app",
+  messagingSenderId: "989017928173",
+  appId: "1:989017928173:web:2b018bbfea5869e64f77e4"
 };
 
 // Inicializar Firebase
