d4c84590d774d9d15054374a586fb97aa267b852
Abunazir44 <silaslinkrock2@gmail.com>
2025-05-09 18:11:21 -0300

    Atualização_10

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js ";
 import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js ";
-
-import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
@@ -16,14 +9,6 @@ import {
   signOut
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js ";
 
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js ";
-
-// 🔧 Substitua pelas suas credenciais do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBKf6qUHaXQOHvgLn6FvIAUgJ1pTzKr6jk",
   authDomain: "instapage-mini-saas.firebaseapp.com",
@@ -33,24 +18,14 @@ const firebaseConfig = {
   appId: "1:989017928173:web:2b018bbfea5869e64f77e4"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
-const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
 
 export {
-  db,
   auth,
-  doc,
-  setDoc,
-  getDoc,
-  GoogleAuthProvider,
+  provider,
   signInWithPopup,
   onAuthStateChanged,
-  signOut,
-  storage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL
+  signOut
 };
